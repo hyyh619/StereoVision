3c60be814f48b0baa72f8a917d257e552c501c6c
Huang Ying <hyyh619@hotmail.com>
2016-06-22 16:58:11 +0800

    StereoVision: swap left and right cameras.
    I reconstruct stereo cameras, so left and right cameras should be
    swapped.
    By: ying.huang.

--- Src/Stereo/Config.h
+++ Src/Stereo/Config.h
@@ -11,8 +11,8 @@ using namespace cv;
 #endif
 
 #ifdef WIN32
-#define TQC_LOGICAL_CAM_LEFT_INDEX      0
-#define TQC_LOGICAL_CAM_RIGHT_INDEX     1
+#define TQC_LOGICAL_CAM_LEFT_INDEX      1
+#define TQC_LOGICAL_CAM_RIGHT_INDEX     0
 #define TQC_INTERNAL_CAM_INDEX          2
 #elif defined(ANDROID)
 #define TQC_LOGICAL_CAM_LEFT_INDEX      0
